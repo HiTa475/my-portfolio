22e9d56cac2de026c5a9efa8e3c26019a4f6efe9
Hitansh <hitanshukineed@gmail.com>
2025-11-10 15:37:51 +0530

    feat:add DOM manipulation to change about text

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ myButton.addEventListener('click', () => {
     alert('You ran your first script!');
 
 });
+
+// --- Text Change Feature ---
+
+// 1. Find the new button
+const changeBtn = document.getElementById('change-text-btn');
+
+// 2. Find the text paragraph
+const aboutText = document.getElementById('about-text');
+
+// 3. Listen for a "click" on the new button
+changeBtn.addEventListener('click', () => {
+
+    // 4. When clicked, change the text content of the paragraph
+    aboutText.textContent = 'You just changed the DOM! This is the core of JavaScript.';
+    
+    // Let's also change the button text so you can't click it twice
+    changeBtn.textContent = 'Changed!';
+
+});
